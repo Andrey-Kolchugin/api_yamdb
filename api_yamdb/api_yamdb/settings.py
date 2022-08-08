d6ad6fa50787b8ab5a1b15a376f5d0fbece8f4db
Гоцецура Иван <storm4724@gmail.com>
2022-08-08 14:56:49 +0300

    Модель user 2.0

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -22,7 +22,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'reviews.apps.IceCreamConfig',
+    'reviews.apps.ReviewsConfig',
     'rest_framework',
     'djoser',
 ]
