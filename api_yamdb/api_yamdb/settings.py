626c548a030473040f94fb396e73b7856df94787
Гоцецура Иван <storm4724@gmail.com>
2022-08-09 11:41:56 +0300

    добавил приложение users

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -22,7 +22,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'api',
-    'reviews.apps.ReviewsConfig',
+    'reviews',
+    'users',
     'rest_framework',
     'djoser',
 ]
--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,55 +1,5 @@
-from django.contrib.auth.models import AbstractUser
 from django.db import models
-
-
-class User(AbstractUser):
-    ADMIN = 'admin'
-    MODERATOR = 'moderator'
-    USER = 'user'
-    ROLES = [
-        (ADMIN, 'admin'),
-        (MODERATOR, 'moderator'),
-        (USER, 'user'),
-    ]
-
-    email = models.EmailField(
-        verbose_name='Адрес электронной почты',
-        unique=True,
-    )
-    username = models.CharField(
-        verbose_name='Имя пользователя',
-        max_length=150,
-        null=True,
-        unique=True
-    )
-    role = models.CharField(
-        verbose_name='Роль',
-        max_length=50,
-        choices=ROLES,
-        default=USER
-    )
-    bio = models.TextField(
-        verbose_name='Биография',
-        null=True,
-        blank=True
-    )
-
-    @property
-    def is_moderator(self):
-        return self.role == self.MODERATOR
-
-    @property
-    def is_admin(self):
-        return self.role == self.ADMIN
-
-    class Meta:
-        ordering = ['id']
-        verbose_name = 'Пользователь'
-        verbose_name_plural = 'Пользователи'
-from django.contrib.auth import get_user_model
-from django.db import models
-
-User = get_user_model()
+from users.models import User
 
 
 class Category(models.Model):
