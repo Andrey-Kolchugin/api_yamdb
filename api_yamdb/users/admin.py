91de75fdb7f248dd1d6400ca8ddd58c407ee2a2f
Гоцецура Иван <storm4724@gmail.com>
2022-08-10 13:44:12 +0300

    добавил админку для Users

--- api_yamdb/users/admin.py
+++ api_yamdb/users/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
 from .models import User
 
-admin.site.register(User)
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'email', 'username', 'role', 'confirmation_code',)
+    search_fields = ('email', 'username',) 
+    list_filter = ('role',)
+    list_editable = ('email', 'username', 'role', 'confirmation_code',)
+    empty_value_display = '-пусто-'
+
+admin.site.register(User, UserAdmin)
